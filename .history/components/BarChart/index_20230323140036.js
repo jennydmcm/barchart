85ca8eca6c761dea287c41cd1313c99b44dc6443
jennydmcm <jenniferdmcmahon@gmail.com>
2023-03-23 14:08:38 -0700

    init

---
+++ .history/components/BarChart/index_20230323140036.js
@@ -0,0 +1,67 @@
+import { usState, useEffect } from 'react'
+import styles from './BarChart/module.css'
+
+import { Bar } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+export default function BarChart() {
+
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+        setChartData({
+            labels: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+            datasets: [
+                {
+                    label: 'Sales $',
+                    data: [12343, 15762, 19832, 13985, 19302, 20567, 21738],
+                    borderColor: 'rbg(53,163,235)',
+                    backgroundColor: 'rgba(53,163,235,0.4)'
+                }
+            ]
+        })
+
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                title: {
+                    display: true,
+                    text: "Daily Revenue"
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        })
+    }, [])
+    return (
+        <>
+
+
+
+        </>
+    )
+}
--- .history/pages/index_20230323134759.js
+++ .history/pages/index_20230323134759.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import BarChart from '@/components/BarChart'
 
 
 export default function Home() {
@@ -14,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <BarChart />
+
       </main>
     </>
   )
